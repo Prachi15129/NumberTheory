4bcfe9292f7eea99131146e1560f21e1f6d9dc81
Prachi15129 <67423563+Prachi15129@users.noreply.github.com>
2021-04-22 14:04:33 +0530

    Create primeFactor.cpp

---
+++ primeFactor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+void primeFactor(int n)
+{
+    vector<pair<int,int>> factors;
+    int cnt;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            cnt=0;
+            while(n%i==0)
+            {
+                n=n/i;
+                cnt++;
+            }
+            factors.push_back(make_pair(i,cnt));
+        }
+    }
+    if(n!=1)
+    {
+        factors.push_back(make_pair(n,1));
+    }
+    for(auto p:  factors)
+    {
+        cout<<p.first<<"^"<<p.second<<",";
+    }
+    return;
+}
+int main() {
+    int n;
+    cin>>n;
+    primeFactor(n);
+    return 0;
+}
